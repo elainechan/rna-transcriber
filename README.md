892eeeadb0642360adb19d5f6c7b01b4922edd89
elainechan <elaine.chan@outlook.com>
2018-01-06 21:25:10 -0500

    Passes first test.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # RNA Transcriber
 
+A practice repo for learning Jest and Redux.
 Translates a DNA string to RNA.
--- rna-conversion.test.js
+++ rna-conversion.test.js
@@ -1,3 +1,5 @@
+const { sayHello } = require('./rna-conversion');
+
 test('For hello world', function() {
 	expect(sayHello()).toBe('Hello world!');
 });
